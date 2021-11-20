4bc2ca60cc0c2f624b8eead303b132efcbd1cf99
Ben Steadman <steadmanben1@gmail.com>
2021-11-20 14:57:12 +0000

    WIP multiple input files
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,71 @@
+#[macro_use]
+extern crate clap;
+
 use clap::{crate_version, App, Arg};
-use sample::sample;
+use sample::reservoir_sample;
 use std::env;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+
+const STDIN_INPUT: &str = "-";
+
+#[derive(Debug)]
+enum Input {
+    File(PathBuf),
+    Stdin,
+}
 
 fn main() {
-    let app = App::new("sample").version(crate_version!()).arg(
-        Arg::with_name("sample-size")
-            .long("sample-size")
-            .short("n")
-            .takes_value(true)
-            .default_value("10"),
-    );
-
+    let app = App::new("sample")
+        .version(crate_version!())
+        .about("Randomly sample lines")
+        .arg(
+            Arg::with_name("FILE")
+                .help("File(s) to sample. Use '-' for standard input.")
+                .long_help("File(s) to sample. Use a dash ('-') or no argument to read from standard input. \
+                    Multiple files are sampled as a single population.")
+                .takes_value(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("sample-size")
+                .long("sample-size")
+                .short("n")
+                .takes_value(true)
+                .default_value("10"),
+        );
     let matches = app.get_matches();
-    let n: u32 = matches
-        .value_of("sample-size")
-        .unwrap()
-        .parse()
-        .expect("invalid sample-size");
+    let n = value_t!(matches, "sample-size", u32).expect("invalid sample-size");
 
-    let stdin = io::stdin();
-    let sampled = sample(stdin.lock().lines().map(|l| l.unwrap()), n);
+    // TODO: Extract all of this and test
+    let inputs = {
+        match matches.values_of_os("FILE").map(|vs| vs.map(Path::new)) {
+            Some(filenames) => filenames
+                .map(|s| {
+                    if s.to_str().unwrap_or_default() == STDIN_INPUT {
+                        Input::Stdin
+                    } else {
+                        Input::File(s.to_path_buf())
+                    }
+                })
+                .collect(),
+            None => vec![Input::Stdin],
+        }
+    };
+
+    let lines = inputs.iter().flat_map(|inp| {
+        let r: Box<dyn io::Read> = match inp {
+            Input::Stdin => Box::new(io::stdin()),
+            Input::File(ref fname) => {
+                let f = File::open(fname).unwrap();
+                Box::new(f)
+            }
+        };
+        io::BufReader::new(r).lines().map(|l| l.unwrap())
+    });
+    let sampled = reservoir_sample(lines, n);
     for l in sampled {
         println!("{}", l);
     }
